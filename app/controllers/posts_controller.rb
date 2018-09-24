f7025b0e5b08b2b5aa555de73f533d41f0f82588
Remi Juhe <remi.juhe@edu.escpeurope.eu>
2018-09-24 15:44:18 +0200

    form page d'accueil

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,5 +10,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(review_params)
+    @post.save
+    redirect_to post_path(@post.id)
+  end
+
+  private
+
+  def review_params
+    params.require(:post).permit([:status, :city, :rent_with_expenses_amount, :user_id, :description, :firstname, :lastname, :property_surface, :coordinates, :address, :pictures, :rooms_count, :zip_code])
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   include PgSearch
   pg_search_scope :search_by_city_and_status,
-    against: [ :city, :status ],
+    against: [ :city, :status, :pictures ],
     using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      tsearch: { prefix: true }
     }
 end
